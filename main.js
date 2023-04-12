78d904a8b45f560fc4b225015b75752b8fec401e
Potham <potham@macbook-pro.home>
2023-04-12 19:30:50 +0200

    ajout bga

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ const mysteryNumber = Math.floor( Math.random() * 500 );
 
 let numberTry = 1;
 
-let userAnswer = askNumber();
+let userAnswer2 = askGame();
+let userAnswer1 = askNumber();
+
+function askGame(){
+    const userGame = prompt("Quel est ton jeu préféré sur BGA ?");
+    // console.log(userNumber1);
+    return userGame;
+}
 
 function askNumber(){
     const userNumber1 = prompt("Donne-moi un nombre entre 0 et 500.");
@@ -11,14 +18,14 @@ function askNumber(){
     return userNumber1;
 }
 
-while (Number(userAnswer) !== mysteryNumber) {
-    if (Number(userAnswer) < mysteryNumber) {
+while (Number(userAnswer1) !== mysteryNumber) {
+    if (Number(userAnswer1) < mysteryNumber) {
         alert("C'est trop petit, retente ta chance !");
     }
     else {
         alert("C'est trop grand, retente ta chance !");
     }
-    userAnswer = askNumber();
+    userAnswer1 = askNumber();
     numberTry = numberTry + 1;
     // console.log('nombre essai', numberTry)
 }
@@ -26,5 +33,5 @@ while (Number(userAnswer) !== mysteryNumber) {
 userWin();
 
 function userWin() {
-    alert("Bravo, tu as gagné en " + numberTry + " essais !");
+    alert("Bravo, tu as gagné en " + numberTry + " essais ! Tu as gagné une partie de " + userAnswer2 + " sur BGA quand tu veux !");
 }
